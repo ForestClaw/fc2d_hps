d682a6da45f7edd7f7e787eaeaa5effad771592e
camperD <DamynChipman@u.boisestate.edu>
2021-08-26 17:09:12 -0600

    Added Google Test for unit testing.

---
+++ tests/fc2d_hps_unittest_hello.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include <fclaw2d_include_all.h>
+#include <fc2d_hps.h>
+
+TEST(Hello, init) {
+	ASSERT_TRUE(true);
+}
+
+// int main(int argc, char** argv) {
+// 	return 0;
+// }
