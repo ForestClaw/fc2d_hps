980938f1baaf9e0e13d955c9882338c8aa80572b
camperD <DamynChipman@u.boisestate.edu>
2021-09-08 11:27:13 -0600

    Adding matrix class and unit tests (passing).

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,11 @@
 #include "gtest/gtest.h"
+#include <string>
 
 int main(int argc, char* argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
+	testing::internal::CaptureStdout();
+	std::string output = testing::internal::GetCapturedStdout();
+	std::cout << "Running tests..." << std::endl;
+	std::cout << output << std::endl;
 	return RUN_ALL_TESTS();
 }
