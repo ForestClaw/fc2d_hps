d961fbbf660bfb98b19597d3b49512e1ea21827e
Damyn Chipman <damyn.chipman@gmail.com>
2021-09-16 16:21:35 -0600

    Beginning work on 4-to-1 merge function and general progress on merge methods.

--- fc2d_hps_merge.cpp
+++ fc2d_hps_merge.cpp
@@ -23,3 +23,37 @@ fc2d_hps_matrix<double> merge_operation_T(fc2d_hps_matrix<double> T_alpha_11, fc
 	return T;
 
 }
+
+void merge_4to1(fc2d_hps_patch& parent, fc2d_hps_patch& child0, fc2d_hps_patch& child1, fc2d_hps_patch& child2, fc2d_hps_patch& child3) {
+
+	// Error checks
+	//    Check if all children are leaves
+	if (child0.is_leaf == false) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child0` is not a leaf"); }
+	if (child1.is_leaf == false) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child1` is not a leaf"); }
+	if (child2.is_leaf == false) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child2` is not a leaf"); }
+	if (child3.is_leaf == false) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child3` is not a leaf"); }
+
+	//    Check if children data are set
+	if (child0.T.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child0.T.size()` is 0; it shouldn't be..."); }
+	if (child1.T.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child1.T.size()` is 0; it shouldn't be..."); }
+	if (child2.T.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child2.T.size()` is 0; it shouldn't be..."); }
+	if (child3.T.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child3.T.size()` is 0; it shouldn't be..."); }
+	if (child0.S.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child0.S.size()` is 0; it shouldn't be..."); }
+	if (child1.S.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child1.S.size()` is 0; it shouldn't be..."); }
+	if (child2.S.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child2.S.size()` is 0; it shouldn't be..."); }
+	if (child3.S.size() == 0) { throw std::invalid_argument("[fc2d_hps_merge merge_4to1] `child3.S.size()` is 0; it shouldn't be..."); }
+
+	// Horizontal merge
+	// fc2d_hps_patch lower_merged();
+	// fc2d_hps_patch upper_merged();
+
+	fc2d_hps_matrix<double> T_alpha = child0.T; // TODO: Write copy constructor for fc2d_hps_matrix<T>
+	fc2d_hps_matrix<double> T_beta = child1.T;
+
+	// ...
+
+	// Vertical merge
+	
+	
+
+}
--- fc2d_hps_merge.hpp
+++ fc2d_hps_merge.hpp
@@ -3,6 +3,7 @@
 
 #include "fc2d_hps_vector.hpp"
 #include "fc2d_hps_matrix.hpp"
+#include "fc2d_hps_patch.hpp"
 
 fc2d_hps_matrix<double> merge_operation_S(
 	fc2d_hps_matrix<double> T_alpha_33,
@@ -17,6 +18,14 @@ fc2d_hps_matrix<double> merge_operation_T(
 	fc2d_hps_matrix<double> T_alpha_13,
 	fc2d_hps_matrix<double> T_beta_23,
 	fc2d_hps_matrix<double> S
+);
+
+void merge_4to1(
+	fc2d_hps_patch& parent,
+	fc2d_hps_patch& child0,
+	fc2d_hps_patch& child1,
+	fc2d_hps_patch& child2,
+	fc2d_hps_patch& child3
 );
 
 #endif // FC2D_HPS_MERGE_HPP
